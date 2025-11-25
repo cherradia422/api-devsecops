1a98dc91906c8bda2dccc466783840012e8b30bd
Ayoub <cherradi.422@ucd.ac.ma>
2025-11-25 17:19:44 +0100

    Add VUNERABILITY

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ export const register = async (req, res) => {
     const existing = await User.findOne({ where: { email } });
     if (existing) return res.status(400).json({ message: "User already exists" });
 
-   //const hashed = await bcrypt.hash(password, 10); 
+
     const hashed = password; // add vun
 
     // Role defaults to 'user' if not provided
