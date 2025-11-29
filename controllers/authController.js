9f743c01c174157634f492dd663efe9c995532aa
Ayoub <cherradi.a422@ucd.ac.ma>
2025-11-29 21:21:45 +0100

    add jwt  secret

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
         { id: user.id, email: user.email, role: user.role },
         process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES }
+        { expiresIn: process.env.JWT_EXPIRES || "2h"  }
     );
 
     res.json({
